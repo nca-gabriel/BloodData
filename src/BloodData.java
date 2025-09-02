01fc2e71b864d392238983b8ae80ab47bad15f2f
Neil <ncagabriel02@gmail.com>
2025-09-02 10:57:12 +0800

    uuhh

--- src/BloodData.java
+++ src/BloodData.java
@@ -1,24 +1,31 @@
-public class BloodData{
-    static String BloodType;
-    static String rhFactor;
+public class BloodData
+{
+    private String BloodType;
+    private String rhFactor;
 
     public BloodData(){ //default constructor
         BloodType = "O";
         rhFactor = "+";
     }
 
-    public BloodData(String bt, String rh){ //overloaded constructor
-        this.BloodType = bt; //instance reference ('this' is an object of the class)
-        this.rhFactor = rh;
-        /*
-        or this, since variable is static
-          BloodType = bt;
-          rhFactor = rh
-        */
-
-    }
-
     public void display(){
         System.out.println(BloodType + rhFactor + " is added on the blood bank");
     }
+
+    public void setBloodType(String bloodType1) {
+        this.BloodType = bloodType1;
+    }
+
+    public void setRhFactor(String rhFactor1) {
+        this.rhFactor = rhFactor1;
+    }
+
+    public String getBloodType(){
+        return BloodType;
+    }
+
+    public String getRhFactor(){
+        return rhFactor;
+    }
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main 
+{
     public static void main(String[] args)
     {
+        BloodData bd = new BloodData();
         Scanner sc = new Scanner(System.in);
-        String bloodType; //global variables to call & store value
-        String rhFactor; //initialized because it cannot be accessed in the while code blocks
         while (true)
         {
             System.out.print("Enter the blood type (O, A, B, AB) of the patient: ");
-            bloodType = sc.nextLine().toUpperCase();
-            if (bloodType.matches("[OAB]|AB|")) { //accepts only A,B,C,O,AB or ""
+            bd.setBloodType(sc.nextLine().toUpperCase());
+            if (bd.getBloodType().matches("[OAB]|AB|")) { //accepts only A,B,C,O,AB or ""
                 break; //breaks the while loop and continue to the second one
             } else {
                 System.out.println("Invalid input. Please use (O, A, B, AB) only.\n");
@@ -20,18 +20,18 @@ public class Main {
         while (true)
         {
             System.out.print("Enter the Rhesus factor (+ or -) of the patient: ");
-            rhFactor = sc.nextLine();
-            BloodData bd;
-            if (bloodType.isEmpty() && ((rhFactor.equals("+") || rhFactor.equals("-")))){ //in case if input is empty bloodtype and have valid rhfactor
-                System.out.println("Invalid input. Please input a bloodtype and an rhfactor.\n");
+            bd.setRhFactor(sc.nextLine());
+            if (bd.getBloodType().isEmpty() && ((bd.getRhFactor().equals("+") || bd.getRhFactor().equals("-")))){
+                //in case if input is empty blood type and have valid rh factor
+
+                System.out.println("Invalid input. Please input a blood type and an rh factor.\n");
                 break;
-            } else if (rhFactor.isEmpty() && bloodType.isEmpty()) {
+            } else if (bd.getBloodType().isEmpty() && bd.getRhFactor().isEmpty()) {
                 bd = new BloodData();
                 bd.display();
                 break;
-            } else if (rhFactor.equals("+") || rhFactor.equals("-")){
-                bd = new BloodData(bloodType, rhFactor);
-                bd.display();
+            } else if (bd.getRhFactor().equals("+") || bd.getRhFactor().equals("-")){
+                System.out.println(bd.getBloodType() + bd.getRhFactor() + " is added to the blood bank");
                 break;
             }else {
                 System.out.println("Invalid input. Please use (+ or -) only.\n");
